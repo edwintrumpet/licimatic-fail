f1f080f4db8a86da696b74b131de78391d9d9ee3
Edwin García <spark.com.co@gmail.com>
2020-06-04 21:48:58 -0500

    Fix test

--- src/__test__/routes.test.js
+++ src/__test__/routes.test.js
@@ -5,7 +5,7 @@ const request = testServer(routes);
 
 describe('Routes', () => {
   describe('/', () => {
-    it('Should return status 200', () => request.get('/').then((response) => expect(response.statusCode).toBe(201)));
+    it('Should return status 200', () => request.get('/').then((response) => expect(response.statusCode).toBe(200)));
     it('Should return a message', () => request.get('/').then((response) => expect(response.body).toEqual({ message: 'Routes works!' })));
   });
 });
